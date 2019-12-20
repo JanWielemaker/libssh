1bae06e670a6fbb5949fee1181f28610eceb68ad
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2019-12-20 11:40:19 +0100

    Updated examples

--- examples/password.pl
+++ examples/password.pl
@@ -1,7 +1,7 @@
 % Assumes this pack is installed as pack
 :- use_module(library(ssh_server)).
 
-% This demo shows using password based authentication. The server is
+% This demo shows using password  based   authentication.  The server is
 % simply started using
 %
 %    ?- ssh_server.
@@ -10,8 +10,9 @@
 %
 %    ?- ssh_server([auth_methods([password])]).
 %
-% and may be accessed using ``ssh -p 2020 localhost``. Of course,
-% normally the passwords are stored on an external file.
+% and may be accessed using  ``ssh   -p  2020  bob@localhost`` using the
+% password _secret_. Of course, normally the  passwords are stored on an
+% external file.
 
 :- multifile
     ssh_server:verify_password/3.
