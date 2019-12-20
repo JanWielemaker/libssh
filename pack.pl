a01b75cbf760373bceb48ff81cc7aa01402e433f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2019-12-20 11:54:51 +0100

    Prepare a release

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(ssh_server).
-version('0.0.1').
-title('Allow SSH into a Prolog process').
+version('0.8.0').
+title('Provide an embedded SSH server').
 keywords([ssh, server, debugging, management]).
 author( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
-home('https://github.com/JanWielemaker/ssh_server' ).
-download( 'https://github.com/JanWielemaker/ssh_server/archive/*.zip' ).
+home('https://github.com/JanWielemaker/libssh' ).
+download('https://github.com/JanWielemaker/libssh/archive/*.zip').
